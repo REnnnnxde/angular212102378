1f1b9f794ffac23d3a12a223ff051bd7df1c91bd
Renn <119943753+REnnnnxde@users.noreply.github.com>
2023-10-23 10:36:58 +0700

    minggu7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
   { path: "admin", component: AdminComponent },
   { path: "login", component: LoginComponent },
+  { path: "dashboard",component:DashboardComponent },
+  { path: "dashboard2",component:Dashboard2Component },
   { path: "", redirectTo: "login", pathMatch: "full" },
   {path:"register", component:RegisterComponent},
 ];
